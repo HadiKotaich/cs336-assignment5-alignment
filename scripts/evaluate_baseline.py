e3ef79b4c75f27d9ce9856b5ad2f974f9e0a80cb
Hadi Kotaich <hadikotaich@meta.com>
2025-11-17 00:04:25 -0800

    added baseline evaluation of qwen 2.5-1.5B
    Summary:
    - used gsm8k dataset, qwen 2.5-1.5B model
    - Latency Statistics:
      Generation latency: 23.12 seconds
      Evaluation latency: 0.61 seconds
    
    - Reward Statistics (avg):
      Format reward: 0.25
      Answer reward: 0.06
      Reward: 0.06

--- scripts/evaluate_baseline.py
+++ scripts/evaluate_baseline.py
@@ -1,72 +1,126 @@
+import json
 import os
-import subprocess
+import time
 from itertools import batched
+from pathlib import Path
 from typing import Callable, List
 
+from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
+
+from datasets import DatasetDict, load_dataset
+
 from datasets.features.features import Sequence
+from pydantic import BaseModel
 from vllm import LLM, SamplingParams
 from vllm.outputs import RequestOutput
 
+# RUN:
 # python scripts/evaluate_baseline.py
 
+# DESCRIPTION:
 # load math validation examples
-
 # format them as prompts to llm
-
 # generate outputs from llm
-
 # calculate evaluation metrics
-
 # serialize examples, model generations, evaluation scores to disk
 
 
-def get_free_gpu():
-    # Query memory via nvidia-smi
-    smi_query = subprocess.check_output(
-        ["nvidia-smi", "--query-gpu=memory.free", "--format=csv,noheader,nounits"]
+class DataRow(BaseModel):
+    question: str
+    answer: str
+
+
+class EvalRow(BaseModel):
+    prompt: str
+    response: str
+    ground_truth: str
+    metrics: dict[str, float]
+
+
+def get_gsm8k_data(split) -> list[DataRow]:
+    dataset: DatasetDict = load_dataset("openai/gsm8k", "main", split=split)
+    return [DataRow(question=row["question"], answer=row["answer"]) for row in dataset]
+
+
+def get_prompts(questions: list[str]) -> list[str]:
+    prompt_path = (
+        Path(__file__).parent.parent / "cs336_alignment" / "prompts" / "r1_zero.prompt"
     )
-    memory_free = [int(x) for x in smi_query.decode("utf-8").strip().split("\n")]
-    best_gpu = memory_free.index(max(memory_free))
-    return best_gpu
-
-
-gpu = get_free_gpu()
-os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+    with open(prompt_path) as f:
+        prompt = f.read()
+    return [prompt.format(question=q) for q in questions]
 
 
 def evaluate_vllm(
     vllm_model: LLM,
     reward_fn: Callable[[str, str], dict[str, float]],
     prompts: List[str],
+    ground_truths: List[str],
     eval_sampling_params: SamplingParams,
 ) -> None:
     """
     Evaluate a language model on a list of prompts,
     compute evaluation metrics, and serialize results to disk.
     """
-    batch_size = 32
+    batch_size = 1024
+    format_reward = 0.0
+    answer_reward = 0.0
+    reward = 0.0
+    start_time = time.time()
+    outputs: list[RequestOutput] = vllm_model.generate(prompts, eval_sampling_params)
+    generation_latency = time.time() - start_time
 
-    for batch in batched(prompts, 32):
-        outputs: list[RequestOutput] = vllm_model.generate(batch, eval_sampling_params)
-        for output in outputs:
-            prompt = output.prompt
+    start_time = time.time()
+    with open("eval_results/baseline_metrics.jsonl", "w") as fout:
+        for prompt, ground_truth, output in zip(prompts, ground_truths, outputs):
             response = output.outputs[0].text
-            print(f"Prompt: {prompt!r}, Response: {response!r}")
+
+            eval_row = EvalRow(
+                prompt=prompt,
+                response=response,
+                ground_truth=ground_truth,
+                metrics=reward_fn(response, ground_truth),
+            )
+
+            format_reward += eval_row.metrics["format_reward"]
+            answer_reward += eval_row.metrics["answer_reward"]
+            reward += eval_row.metrics["reward"]
+            fout.write(json.dumps(eval_row.model_dump()) + "\n")
+    eval_latency = time.time() - start_time
+
+    print(f"\nLatency Statistics:")
+    print(f"  Generation latency: {generation_latency:.2f} seconds")
+    print(f"  Evaluation latency: {eval_latency:.2f} seconds")
+
+    print(f"\nReward Statistics:")
+    print(f"  Format reward: {format_reward / len(prompts):.2f}")
+    print(f"  Answer reward: {answer_reward / len(prompts):.2f}")
+    print(f"  Reward: {reward / len(prompts):.2f}")
 
 
-qwen3_1_7b = LLM(model="Qwen/Qwen3-1.7B", dtype="auto")
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+def extract_gsm8k_answer(gsm8k_answer: str) -> str:
+    if "####" in gsm8k_answer:
+        return gsm8k_answer.split("####")[-1].strip()
+    return gsm8k_answer
+
+
+# qwen3_1_7b = LLM(model="Qwen/Qwen3-1.7B")
+model = LLM(model="Qwen/Qwen2.5-1.5B")
+data_rows: list[DataRow] = get_gsm8k_data(split="test")
+prompts: list[str] = get_prompts([row.question for row in data_rows])
+ground_truths: list[str] = [extract_gsm8k_answer(row.answer) for row in data_rows]
 sampling_params = SamplingParams(
-    temperature=1.0, top_p=1.0, max_tokens=1024, stop=["\n"]
+    temperature=1.0,
+    top_p=1.0,
+    max_tokens=1024,
+    stop=["</answer>"],
+    include_stop_str_in_output=True,
 )
+
 evaluate_vllm(
-    vllm_model=qwen3_1_7b,
-    reward_fn=None,
+    vllm_model=model,
+    reward_fn=r1_zero_reward_fn,
     prompts=prompts,
+    ground_truths=ground_truths,
     eval_sampling_params=sampling_params,
 )
--- scripts/run_evaluate_baseline.sh
+++ scripts/run_evaluate_baseline.sh
@@ -4,6 +4,18 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="$( cd "$SCRIPT_DIR/.." && pwd )"
 
+# Select GPU with most free memory
+GPU_ID=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits | \
+    awk '{print NR-1 " " $0}' | \
+    sort -k2 -nr | \
+    head -n1 | \
+    awk '{print $1}')
+
+echo "Memory free on all GPUs:"
+nvidia-smi --query-gpu=index,memory.free --format=csv,noheader,nounits
+echo "Using GPU $GPU_ID"
+
+export CUDA_VISIBLE_DEVICES=$GPU_ID
 export LD_PRELOAD="$PROJECT_DIR/.venv/lib/python3.12/site-packages/nvidia/cublas/lib/libcublas.so.12"
 
 source "$PROJECT_DIR/.venv/bin/activate"
